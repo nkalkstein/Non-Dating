03e20f1c62fa5dcc99e31c3d4c6e9e664259af6e
nkalkstein <nkalkstein@gmail.com>
2018-10-25 22:36:52 -0400

    Added notes on app design

--- Non_Dating_Frontend/non-dating/src/App.js
+++ Non_Dating_Frontend/non-dating/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 //Display many different, very specific "make or break" attributes (physical, personality, or other).
 //Weigh matching attributes against non-matching and product a score?
 // Specify the matching and non-matcbing attributes?
-//No photos necessary?
-
+// No photos necessary?
+// Anonymously show photos and have attractiveness (maybe different qualitites?) voted on, for final score.
 
 class App extends Component {
   render() {
