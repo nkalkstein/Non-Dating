2adcd78054e26c332b6bcfff72ac473fd860ab6d
nkalkstein <nkalkstein@gmail.com>
2018-10-29 15:07:53 -0400

    cleaned up template

--- Non_Dating_Frontend/non-dating/src/index.js
+++ Non_Dating_Frontend/non-dating/src/index.js
@@ -6,6 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+//Root file- User UD, Rating UD, etc. 
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
